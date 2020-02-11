e2e96357876b4017fd184adb199c6f17415e9fc8
Lingchao Xin <douglarek@gmail.com>
2020-02-11 13:00:50 +0800

    82. 删除排序链表中的重复元素 II

---
+++ src/test/java/xin/lingchao/java/linked_list/_082/SolutionTest.java
@@ -0,0 +1,22 @@
+package xin.lingchao.java.linked_list._082;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import xin.lingchao.java.structure.ListNode;
+
+public class SolutionTest {
+    @Test
+    public void testDeleteDuplicates() {
+        Solution s = new Solution();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(3);
+
+        ListNode node = s.deleteDuplicates(head);
+        assertTrue(node.val == 2);
+        assertTrue(node.next.val == 3);
+    }
+}
