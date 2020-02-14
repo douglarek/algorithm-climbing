3e2934f20249447caad9ff7815caaa27fc3a265c
Lingchao Xin <douglarek@gmail.com>
2020-02-14 20:31:38 +0800

    100. 相同的树

---
+++ src/test/java/xin/lingchao/java/tree/_100/SolutionTest.java
@@ -0,0 +1,24 @@
+package xin.lingchao.java.tree._100;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import xin.lingchao.java.structure.TreeNode;
+
+public class SolutionTest {
+    @Test
+    public void testIsSameTree() {
+        Solution s = new Solution();
+
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(1);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(1);
+        q.right = new TreeNode(2);
+
+        assertFalse(s.isSameTree(p, q));
+    }
+}
