c1b868dc630363935122c43c9193f473dc72fbcb
Lingchao Xin <douglarek@gmail.com>
2020-02-15 22:05:31 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ src/test/java/xin/lingchao/java/tree/_108/SolutionTest.java
@@ -0,0 +1,20 @@
+package xin.lingchao.java.tree._108;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import xin.lingchao.java.structure.TreeNode;
+
+public class SolutionTest {
+    @Test
+    public void testSortedArrayToBST() {
+        Solution s = new Solution();
+        TreeNode root = s.sortedArrayToBST(new int[] { -10, -3, 0, 5, 9 });
+        assertTrue(root.val == 0);
+        assertTrue(root.left.val == -10);
+        assertTrue(root.left.right.val == -3);
+        assertTrue(root.right.val == 5);
+        assertTrue(root.right.right.val == 9);
+    }
+}
