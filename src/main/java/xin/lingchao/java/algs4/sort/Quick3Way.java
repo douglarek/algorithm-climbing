226b21793812448665cc8168c68598c7aaa3b527
Lingchao Xin <douglarek@gmail.com>
2020-03-08 10:10:23 +0800

    Algs4 Quick3Way partition comment

--- src/main/java/xin/lingchao/java/algs4/sort/Quick3Way.java
+++ src/main/java/xin/lingchao/java/algs4/sort/Quick3Way.java
@@ -16,13 +16,13 @@ public class Quick3Way implements Sort {
         int v = a[lo];
         while (i <= gt) {
             if (a[i] < v) {
-                swap(a, lt++, i++);
+                swap(a, lt++, i++); // a[lo...lt-1] 中的元素都小于 v
             } else if (a[i] > v) {
-                swap(a, i, gt--);
+                swap(a, i, gt--); // a[gt+1...hi] 中的元素都大于 v
             } else {
-                i++;
+                i++; // a[lt...i-1] 中的元素都等于 v
             }
-        }
+        } // 切分后 a[lo...lt-1] < v = a[lt...gt] < a[gt+1...hi] 成立
 
         sort(a, lo, lt - 1);
         sort(a, gt + 1, hi);
