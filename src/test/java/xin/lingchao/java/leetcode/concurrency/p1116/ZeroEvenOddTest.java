f390f386eeaff4ecbb9d9bf035f27cf8b44609a7
Lingchao Xin <douglarek@gmail.com>
2020-03-20 23:42:41 +0800

    1116. 打印零与奇偶数

---
+++ src/test/java/xin/lingchao/java/leetcode/concurrency/p1116/ZeroEvenOddTest.java
@@ -0,0 +1,41 @@
+package xin.lingchao.java.leetcode.concurrency.p1116;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.jupiter.api.Test;
+
+public class ZeroEvenOddTest {
+    @Test
+    public void testZeroEvenOdd() throws InterruptedException {
+        ZeroEvenOdd zeo = new ZeroEvenOdd(5);
+        CountDownLatch c = new CountDownLatch(3);
+
+        new Thread(() -> {
+            try {
+                zeo.zero(x -> System.out.print(x));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            c.countDown();
+        }).start();
+        new Thread(() -> {
+            try {
+                zeo.even(x -> System.out.print(x));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            c.countDown();
+        }).start();
+        new Thread(() -> {
+            try {
+                zeo.odd(x -> System.out.print(x));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            c.countDown();
+        }).start();
+
+        c.await();
+        System.out.println();
+    }
+}
