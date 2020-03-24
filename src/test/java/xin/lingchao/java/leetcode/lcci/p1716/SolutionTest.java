57ccff3cd22682feb2e7e2dfa788c0a3210b7b85
Lingchao Xin <douglarek@gmail.com>
2020-03-24 09:09:24 +0800

    面试题 17.16. 按摩师

---
+++ src/test/java/xin/lingchao/java/leetcode/lcci/p1716/SolutionTest.java
@@ -0,0 +1,13 @@
+package xin.lingchao.java.leetcode.lcci.p1716;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+    @Test
+    public void testMessage() {
+        Solution s = new Solution();
+        assertTrue(s.massage(new int[] {2, 7, 9, 3, 1}) == 12);
+        assertTrue(s.massage(new int[] {1, 2, 3, 1}) == 4);
+    }
+}
